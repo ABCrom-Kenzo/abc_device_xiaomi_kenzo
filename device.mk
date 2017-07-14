8640bc496a254cc7e0ed4acb6c621ca06b4afe06
Wight554 <wight554@gmail.com>
2017-07-14 12:38:08 +0000

    LOS-like ramdisk

--- device.mk
+++ device.mk
@@ -31,9 +31,8 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     vold.fstab \
     init.class_main.sh \
-    init.qcom.post_boot.sh \
+    init.qcom.power.rc \
     init.qcom.qseecomd.sh \
-    init.qcom.zram.sh \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
