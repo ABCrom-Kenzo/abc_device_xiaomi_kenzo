71fb793ac544da43ac8e8d60136c8f54909e0397
Offain <fabian.leutenegger@bluewin.ch>
2017-08-21 06:34:16 +0000

    kenzo: add QPerformance.jar to boot jars and pinner service
    * add QPerformance to boot jars
    * add QPerformance to the Pinner configuration to ensure the best latency for the userspace boosting framework.

--- device.mk
+++ device.mk
@@ -189,6 +189,9 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVdecHevc \
     libOmxVenc
+	
+# QPerformance
+PRODUCT_BOOT_JARS += QPerformance
 
 # Misc dependency packages
 PRODUCT_PACKAGES += \
